4b7255e0ea9e360dacc716d8ea62ac3f5b06f7eb
godzilla <EliGolts@gmail.com>
2019-06-25 13:37:01 +0300

    Firts commit created project

---
+++ src/main/java/com/eligolts/mishpahug/projectsocialnetwork/ProjectsocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.eligolts.mishpahug.projectsocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsocialnetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsocialnetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eligolts/mishpahug/projectsocialnetwork/ProjectsocialnetworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eligolts.mishpahug.projectsocialnetwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectsocialnetworkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
