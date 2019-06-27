6f5b76f3d35762fbbeca579c21f49ab8d1bdbcea
shatikc <38255933+shatikc@users.noreply.github.com>
2019-06-27 14:40:37 +0300

    Create new file and added print function
    Hello!

---
+++ src/main/java/com/eligolts/mishpahug/projectsocialnetwork/url/AnotherUserCode.java
@@ -0,0 +1,12 @@
+public class AnotherUserCode {
+
+private final String message = "Hello! What's up?"
+
+public void printMessage(){
+    System.out.println(message);
+}
+
+public AnotherUserCode(){
+}
+
+}
